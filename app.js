5a07f9b898ef4b1321840adf5756221333d04a07
Yanghwa <kellerj87@hotmail.com>
2017-03-15 16:54:21 -0400

    added the pictures and added package dependancies

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//passport dependencies
+let passport = require('passport');
+let session = require('express-session');
+let localStrategy = require('passport-local').Strategy;
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// use mongoose to connect to mongodb
+var mongoose = require('mongoose');
+var conn = mongoose.connection;
+
+// link to config file
+var globals = require('./config/globals');
+
+conn.open(globals.db);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +35,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//configure passport and sessions
+app.use(session({
+	secret: 'randomely session secret',
+	resave: true,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//link to the new Account model
+let Account = require('./models/account');
+passport.use(Account.createStrategy());
+
+//facebook auth
+let FacebookStrategy = require('passport-facebook').Strategy;
+passport.use(new FacebookStrategy({
+        clientID: globals.facebook.clientID,
+        clientSecret: globals.facebook.clientSecret,
+        callbackURL: globals.facebook.callbackURL,
+        profileFields: ['id', 'displayName', 'emails']
+    }, function(accessToken, refreshToken, profile, cb) {
+        Account.findOrCreate({ username: profile.displayName}, function(err, user) {
+          return cb(err, user);
+        });
+    }
+));
+
+//manage user login status through the db
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use('/', index);
 app.use('/users', users);
@@ -40,7 +86,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+  	title: 'COMP2068 - Book Store',
+  	user: req.user
+  });
 });
 
 module.exports = app;
