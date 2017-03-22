2d99a199aabcc5dc77dd62f6dc66b848b47cdd88
Yanghwa <kellerj87@hotmail.com>
2017-03-21 20:16:41 -0400

    fix google map, remove codes not used

--- config/globals.js
+++ config/globals.js
@@ -2,7 +2,6 @@
 module.exports = {
 	//mongodb connection
     db: 'mongodb://Yanghwa:wjdfmd8412@ds145639.mlab.com:45639/junghwandb',	
-    // db: 'mongodb://w2017dba:********@ds145669.mlab.com:45669/comp2068-w2017'  // mlab
     //facebook auth
     facebook: {
     	clientID: '195980897527589',
